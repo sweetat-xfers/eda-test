9feac5ecc52bac9a88f52ae1509a1bc9c87fa3af
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-09 21:35:05 +0800

    Adding phobos cg2 pretrade check configuration

---
+++ kafka/cg2-pretradecheck/pre-trade-checks-handler.rb
@@ -0,0 +1,29 @@
+require "json"
+require "phobos"
+
+class PreTradeChecksHandler
+    include Phobos::Handler
+    include Phobos::Producer
+
+    def consume(payload, _metadata)
+        Phobos.logger.info("consume.(#{payload}).(#{_metadata})")
+        # Parse JSON
+        txn = JSON.parse(payload)
+        Phobos.logger.info("txn.(#{txn["amt"].to_f})")
+        if txn["amt"].to_f < 100.0
+            producer.async_publish(
+                topic: "txn.pretrade.ok", 
+                payload: "#{payload}",
+                )
+            Phobos.logger.info("txn.pretrade.ok.(#{payload}).(#{_metadata})")
+        else
+            txn[:errors] = {'msg': 'too.low'}
+            rej_payload = txn.to_json
+            producer.async_publish(
+                topic: "txn.pretrade.rejected", 
+                payload: "#{rej_payload}",
+                )
+            Phobos.logger.info("txn.pretrade.rejected.(#{rej_payload}).(#{_metadata})")
+        end
+    end
+  end
