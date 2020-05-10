ec427e2fe883d3ccec47c353456c09a9420c14f2
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-10 18:33:22 +0800

    Ensuring that the program raises the rollback if it fails to publish to the producer

--- kafka/cg1-mysqlsink/sink-mysql-handler.rb
+++ kafka/cg1-mysqlsink/sink-mysql-handler.rb
@@ -10,10 +10,11 @@ require_relative 'app/models/txn'
 
 class SinkMysqlHandler
   include Phobos::Handler
+  include Phobos::Producer
 
   def self.start(kafka_client)
     db_url = ENV["DATABASE_URL"] ||= "mysql2://xfers:password@mysql:3306/edatest"
-    Phobos.logger.info("db_adapter.(#{db_adapter}).db_url.(#{db_url})")
+    Phobos.logger.info("db_url.(#{db_url})")
 
     ActiveRecord::Base.establish_connection(db_url)
     Phobos.logger.info("self.start.establish_connection(#{db_url})")
@@ -22,13 +23,30 @@ class SinkMysqlHandler
   def consume(payload, _metadata)
     Phobos.logger.info "consume(#{payload}),(#{_metadata})"
     txn = Txn.new.from_json(payload)
-    Phobos.logger.info "Txn.new.from_json(#{txn})"
+    Phobos.logger.info "txn.new.from_json(#{txn.to_json})"
 
     ActiveRecord::Base.transaction do
-      unless txn.save
+      if !txn.id and txn.save
+        # We want the producer here for txn.created because
+        # we need a txn.id to associate processors downstream
+        txn_msg = txn.to_json
+        Phobos.logger.info "Txn.save(#{txn_msg})"
+        begin
+          producer.async_publish(
+              topic: "txn.created", 
+              payload: "#{txn_msg}",
+              )
+          Phobos.logger.info "txn.created(#{txn_msg})"
+        rescue 
+          raise ActiveRecord::Rollback, "txn.created.failed(#{txn_msg})"
+        end
+      else
+        # If it cannot save, we probably want to retry again.
         txn.errors.messages.each do |field, messages|
           Phobos.logger.error "save.errors(#{txn}).(#{field}: #{messages})"
         end
+        # If we throw an exception, the handler will try again
+        raise ActiveRecord::Rollback, "txn.save.failed(#{txn.errors})"
       end
     end
   end
