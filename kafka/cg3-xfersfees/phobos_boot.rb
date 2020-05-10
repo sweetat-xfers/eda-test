dc5742a046a4ab0da792ad1db49b9de759c67bad
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-10 21:10:05 +0800

    Adding cg3-xfersfees

---
+++ kafka/cg3-xfersfees/phobos_boot.rb
@@ -0,0 +1,14 @@
+# Use this file to load your code
+puts <<~ART
+  ______ _           _
+  | ___ \\ |         | |
+  | |_/ / |__   ___ | |__   ___  ___
+  |  __/| '_ \\ / _ \\| '_ \\ / _ \\/ __|
+  | |   | | | | (_) | |_) | (_) \\__ \\
+  \\_|   |_| |_|\\___/|_.__/ \\___/|___/
+ART
+puts "
+phobos_boot.rb - find this file at #{File.expand_path(__FILE__)}
+
+"
+require_relative "xfers-fees-handler"
