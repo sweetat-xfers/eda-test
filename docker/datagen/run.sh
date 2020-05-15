da1bbf843f5cef8074ec379a6bc0224aa65c2442
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-15 21:56:40 +0800

    Removing datagen

--- docker/datagen/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## This is for the datagen to generate insane numbers of data for testing
-curl -X POST \
-  -H "Content-Type: application/json" \
-  --data @connector_stock_trades.config \
-  http://localhost:8083/connectors
